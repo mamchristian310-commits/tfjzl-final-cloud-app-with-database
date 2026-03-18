44f496323037451f83ef8e9f17fbc2a3e4cc5a6c
Mam Christian <mamchristian310@gmail.com>
2026-03-18 12:00:39 +0100

    Registration of Course, Question and Submission models

--- onlinecourse/admin.py
+++ onlinecourse/admin.py
@@ -1,13 +1,27 @@
 from django.contrib import admin
 # <HINT> Import any new Models here
-from .models import Course, Lesson, Instructor, Learner
+from .models import *
 
 # <HINT> Register QuestionInline and ChoiceInline classes here
+
+class QuestionInline(admin.StackedInline):
+    model = Question
+    extra = 2
+
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 2
 
 
 class LessonInline(admin.StackedInline):
     model = Lesson
     extra = 5
+
+class QuestionAdmin(admin.ModelAdmin):
+    inlines = [ChoiceInline]
+    list_display = ['question_text']
+    list_filter = ['course']
+    search_fields = ['question_text']
 
 
 # Register your models here.
@@ -28,3 +42,6 @@ admin.site.register(Course, CourseAdmin)
 admin.site.register(Lesson, LessonAdmin)
 admin.site.register(Instructor)
 admin.site.register(Learner)
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice)
+admin.site.register(Submission)
